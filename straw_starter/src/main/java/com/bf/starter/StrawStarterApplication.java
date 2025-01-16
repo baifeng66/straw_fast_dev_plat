8f0ca9d72257703cd116209317e2204b47cc3dac
chb <2572907586@qq.com>
2025-01-16 16:34:51 +0800

    初始化-稻草快速开发平台

---
+++ straw_starter/src/main/java/com/bf/starter/StrawStarterApplication.java
@@ -0,0 +1,16 @@
+package com.bf.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: bf
+ * @time: 2025/1/16 15:42
+ * @description:
+ */
+@SpringBootApplication
+public class StrawStarterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StrawStarterApplication.class, args);
+    }
+}
